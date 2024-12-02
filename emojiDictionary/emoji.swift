dc7858222e41853f85ac656fca3885ef303f0439
Sachin <tarkasachin842@gmail.com>
2024-12-02 12:13:58 +0530

    Initial Commit

---
+++ emojiDictionary/emoji.swift
@@ -0,0 +1,14 @@
+//
+//  emoji.swift
+//  emojiDictionary
+//
+//  Created by Batch - 2 on 29/11/24.
+//
+
+import Foundation
+struct Emoji{
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
